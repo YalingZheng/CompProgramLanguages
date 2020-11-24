b9a6e7a81ff8063c69a06f836890da17342657a5
YalingZheng <yaling.zheng@gmail.com>
2020-11-24 17:47:49 -0500

    Create GradeDist.cpp

---
+++ ExceptionHandling/GradeDist.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+int main()
+{
+    int new_grade, index, limit_1, limit_2, freq[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+    // The exception definition to deal with the end of the data
+    class NegativeInputException {
+    public:
+        NegativeInputException() {
+            std::cout << "End of input data reached\n";
+        }
+    };
+
+    try {
+        while (true) {
+            std::cout << "Please input a grade\n";
+            std::cin>>new_grade;
+            if (!new_grade) {
+                std::cout<<"NOT A NUMBER!";
+                break;
+             }
+            if (new_grade < 0)
+                throw NegativeInputException();
+            index = new_grade / 10;
+            try {
+                if (index > 9)
+                    throw new_grade;
+                freq[index]++;
+            } // end of inner try 
+            catch (int grade) {
+                if (grade == 100)
+                    freq[9]++;
+                else
+                    std::cout << "Error -- new grade:" << grade << " is out of range [1, 100]\n";
+            } // end of inner catch 
+        } // end of while
+
+    } // end of outer try
+    catch (NegativeInputException e) {
+        std::cout << " Limits   Frequency\n";
+        for (index = 0; index < 10; index++) {
+            limit_1 = 10 * index;
+            limit_2 = limit_1 + 9;
+            if (index == 9) {
+                limit_2 = 100;
+            } // end of if
+        std::cout << limit_1 <<"-" << limit_2 <<"   " << freq[index] << "\n";
+        } // end of for
+    } // end of catch NegativeInputException
+    // DOES C++ sppport finally blocks?
+    // No, C++ does not support 'finally' blocks. 
+} // end of main
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
